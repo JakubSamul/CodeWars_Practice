073dd60550531bd91564f73c7673a3e116c48108
Jakub Samul <jakubsamul0@gmail.com>
2022-05-05 21:07:10 +0200

    7

--- Codewar07.py
+++ Codewar07.py
@@ -1,2 +1,17 @@
 
+import string
+
+def printer_error(s):
+    chars = string.ascii_lowercase
+    good = list(chars)[chars.find('a'):chars.find('m')+1]
+    count = 0
+
+    for c in s:
+        if c not in good:
+            count += 1
+    return str(count) + "/" + str(len(s))
+
+print(printer_error("aaabbbmmmddd"))
+print(printer_error("aaabbzzzzzbmmmyydddx"))
+
 
