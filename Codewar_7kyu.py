dec82932c1d78ff05896bfd678996e6198f63414
Jakub Samul <jakubsamul0@gmail.com>
2022-09-30 11:22:40 +0200

    7kyu

---
+++ Codewar_7kyu.py
@@ -0,0 +1,17 @@
+# 7 kyu
+
+
+# Sum of the first nth term of Series
+
+# def series_sum(n):
+#     result = 1
+#     if 0 <= n <= 1:
+#         return str(n) + ".00"
+#     else:
+#         for i in range(1, n):
+#             s = 1 / (1 + (3*i))
+#             result += s
+#         return "{:.2f}".format(result)
+
+
+# print(series_sum(32))
