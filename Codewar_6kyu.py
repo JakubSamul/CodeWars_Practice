e4e8e45fe76b18e58650e1540d654b5d4dc2fc84
Jakub Samul <jakubsamul0@gmail.com>
2022-10-18 15:42:28 +0200

    cd

--- Codewar_6kyu.py
+++ Codewar_6kyu.py
@@ -1,5 +1,4 @@
 # # Title Case
-
 # def title_case(title, minor_words=''):
 #     result = ""
 #     MinorWords = ""
@@ -16,23 +15,46 @@
 #     result = result[0:len(result)-1]
 #     first = result[0] if result else ""
 #     return result.replace(first, first.upper(), 1)
-
-
-
 # print(title_case('First a of in', ''))
 
+
+
 # Simple Encryption #1 - Alternating Split
+# def encrypt(encrypted_text, n):
+#     for i in str(n):
+#         step1 = []
+#         step2 = []
+#         for step in range(1,len(encrypted_text)+1,2):
+#             step1.append(encrypted_text[step])
+#         for step in range(0,len(encrypted_text)+1,2):
+#             step2 += encrypted_text[step]
+#     print(step1)
 
-def encrypt(encrypted_text, n):
-    for i in str(n):
-        step1 = []
-        step2 = []
-        for step in range(1,len(encrypted_text)+1,2):
-            step1.append(encrypted_text[step])
-        for step in range(0,len(encrypted_text)+1,2):
-            step2 += encrypted_text[step]
-    print(step1)
+# print(encrypt("This is a test!", 1))
 
-print(encrypt("This is a test!", 1))
 
+
+# Help the bookseller !
+# b = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"]
+# c = ["A", "B", "C", "D"]
+# def stock_list(listOfArt, listOfCat):
+#     final = ""
+#     result = []
+#     for i in range(len(listOfCat)):
+#         result.append(listOfCat[i])
+#         for j in range(len(result)):
+#             result[j] = 0
+#     for a in listOfArt:
+#         a = a.split(" ")
+#         cat = a[0]
+#         for i in range(0,len(listOfCat)):
+#             if cat[0] == listOfCat[i]:
+#                 result[i] += int(a[1])
+#     for c in range(len(result)):
+#         final += "(" + listOfCat[c] + " : " + str(result[c]) + ") - "
+#     if sum(result) == 0:
+#         return ""
+#     else:
+#         return final[0:-3]
+# print(stock_list(b,c))
 
--- Codewar_7kyu.py
+++ Codewar_7kyu.py
@@ -1,8 +1,4 @@
-# 7 kyu
-
-
 # Sum of the first nth term of Series
-
 # def series_sum(n):
 #     result = 1
 #     if 0 <= n <= 1:
@@ -12,27 +8,24 @@
 #             s = 1 / (1 + (3*i))
 #             result += s
 #         return "{:.2f}".format(result)
-
-
 # print(series_sum(32))
 
-# Get the Middle Character
 
+
+# Get the Middle Character
 # def get_middle(s):
 #     l = int(len(s) / 2)
 #     if len(s) % 2 == 0:
 #         return s[l-1:l+1]
 #     else:
 #         return s[l]
-
-
 # print(get_middle("testtest"))
 
-# Maximum Length Difference
 
+
+# Maximum Length Difference
 # s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
 # s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
-
 # def mxdiflg(a1, a2):
 #     result = []
 #     if a1 != [] and a2 != []:
@@ -43,5 +36,15 @@
 #         return max(result)
 #     else:
 #         return -1
-
 # print(mxdiflg(s1,s2))
+
+
+
+# Simple Fun #176: Reverse Letter
+# def reverse_letter(string):
+#     result = ""
+#     for text in string[::-1]:
+#         if text.isalpha() == True:
+#             result += text
+#     return result
+# print(reverse_letter("ultr53o?n"))
