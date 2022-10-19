3eb18b4d42d9c3dec4a02aff9bcb1919b117f667
Jakub Samul <jakubsamul0@gmail.com>
2022-10-19 18:08:23 +0200

    cd

--- Codewars_5kyu.py
+++ Codewars_5kyu.py
@@ -1,43 +1,31 @@
-# # Sum of Pairs
+# # # Sum of Pairs
+# l1 = [1, 4, 8, 7, 3, 15]
+# l2 = [1, -2, 3, 0, -6, 1]
+# l4 = [1, 2, 3, 4, 1, 0]
+# s = 8
+# s1 = -6
+# s2 = 2
 # def sum_pairs(ints, s):
-#     result = []
-#     lenght = len(ints)
-#     for a in ints[:lenght-1]:
-#         step = 1
-#         for b in ints[step+1:]:
-#             if a + b == s:
-#                 if result == []:
-#                     result.append(a)
-#                     result.append(b)
-#                 else:
-#                     c = result[1]
-#                     if ints.index(b) < ints.index(c):
-#                         result[0] = a
-#                         result[1] = b
-#         step += 1
-#     if result == []:
-#         return None
-#     else:
-#         return result
+#     myset = set()
+#     for num in ints:
+#         required = s - num
+#         if required in myset:
+#             return [required, num]
+#         myset.add(num)
+# print(sum_pairs(l4,s2))
+                
 
 
 
 # Directions Reduction
-a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
-def dirReduc(arr):
-    for a in arr:
-        for b in arr[1:]:
-            if a == "NORTH" and b == "SOUTH":
-                del arr[arr.index(a)]
-                del arr[arr.index(b)]
-            elif a == "SOUTH" and b == "NORTH":
-                del arr[arr.index(a)]
-                del arr[arr.index(b)]
-            elif a == "WEST" and b == "EAST":
-                del arr[arr.index(a)]
-                del arr[arr.index(b)]
-            elif a == "EAST" and b == "WEST":
-                del arr[arr.index(a)]
-                del arr[arr.index(b)]
-    return arr
-print(dirReduc(a))
+# a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+# def dirReduc(arr):
+#     dict = {"NORTH":"SOUTH","SOUTH":"NORTH","EAST":"WEST","WEST":"EAST"}
+#     res = []
+#     for i in arr:
+#         if res and dict[i] == res[-1]:
+#             res.pop()
+#         else:
+#             res.append(i)
+#     return res
+# print(dirReduc(a))
--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -77,3 +77,23 @@
 #             a = result.index(i)
 #             return seq[a]
 # print(find_it(a))
+
+
+
+# Count the smiley faces!
+# a = [':)',':(',':D',':O',':;']
+# b = [':D',':~)',';~D',':)']
+# def count_smileys(arr):
+#     count = 0
+#     for i in arr:
+#         if len(i) == 2:
+#             if i[0] == ";" or i[0] == ":":
+#                 if i[1] == ")" or i[1] == "D":
+#                     count += 1
+#         if len(i) == 3:
+#             if i[0] == ";" or i[0] == ":":
+#                 if i[1] == "-" or i[1] == "~":
+#                     if i[2] == ")" or i[2] == "D":
+#                         count += 1
+#     return count    
+# print(count_smileys((b)))
