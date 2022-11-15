d5fa8cd6c896b34384b1c3110be3d31d5fd79fbb
Jakub Samul <jakubsamul0@gmail.com>
2022-11-15 16:04:16 +0100

    '4kyu-strings-mix'

--- Codewars_4kyu.py
+++ Codewars_4kyu.py
@@ -1,6 +1,30 @@
-# Strings Mix
-def mix(s1, s2):
-    for i in s1:
-        a = s1.count(i)
-        print(a)
-print(mix("Are they here", "yes, they are here"))
+# # Strings Mix
+# a1 = "Sadus:cpms>orqn3zecwGvnznSgacs"
+# a2 = "MynwdKizfd$lvse+gnbaGydxyXzayp"
+# m1 = "Are they here"
+# m2 = "yes, they are here"
+# from collections import Counter
+
+# def mix(a1, a2):
+#     first = Counter(a1)
+#     secound = Counter(a2)
+#     result = []
+#     for k, v in first.items():
+#         if secound.get(k,0) == v:
+#             result.append((k,v,3))
+#         elif secound.get(k, 0) > v:
+#             result.append((k, secound.pop(k),2))
+#         else:
+#             result.append((k,v,1))
+#     for k, v in secound.items():
+#         if k not in first:
+#             result.append((k,v,2))
+#     result = sorted(result, key = lambda x: (-x[1], x[2], x[0]))
+#     data = ''
+#     for k,v,i in result:
+#         if v > 1 and k.isalpha() and k != k.upper():
+#             data += f"{'=' if i == 3 else i}:{v*k}/"
+#     if data:
+#         data = data.strip('/')
+#     return data
+# print(mix(a1,a2))
