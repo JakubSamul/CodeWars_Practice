92b711c3fda189386daecaa4a1bb5d794fb183e6
Jakub Samul <jakubsamul0@gmail.com>
2022-12-13 22:56:59 +0100

    '6kata-Your-order-please'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -188,3 +188,14 @@
 #             final.append(a)
 #     return final
 # print(data_reverse(data1))
+
+
+
+# Your order, please
+# def order(sentence):
+#     words = []
+#     for i in range(1,10):
+#         for word in sentence.split():
+#             if str(i) in word:
+#                 words.append(word)
+#     return " ".join(words)
