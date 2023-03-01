dd4530f6c39c7731f426bba1478633647b2bf230
Jakub Samul <jakubsamul0@gmail.com>
2023-03-01 23:16:03 +0100

    'The-Vowel-Code'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -199,3 +199,16 @@
 #             if str(i) in word:
 #                 words.append(word)
 #     return " ".join(words)
+
+
+
+# The Vowel Code
+# vow = {"a":"1", "e":"2", "i":"3", "o":"4", "u":"5"}
+# def encode(st):
+#     for w in vow:
+#         st = st.replace(w, vow[w])
+#     return st
+# def decode(st):
+#     for k,v in vow.items():
+#         st = st.replace(v, k)
+#     return st
