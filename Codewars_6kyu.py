407d9bae310c85d62f06962e0b28c4d2d9ad126c
Jakub Samul <jakubsamul0@gmail.com>
2023-03-12 22:24:24 +0100

    '6kata-Duplicate-Encoder'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -219,3 +219,9 @@
 # def alphabet_position(text):
 #     alp = "abcdefghijklmnopqrstuvwxyz"
 #     return " ".join([str(alp.find(c) + 1) for c in text.lower() if c in alp])
+
+
+
+# Duplicate Encoder
+# def duplicate_encode(word):
+#     return "".join(["(" if word.lower().count(x) == 1 else ")" for x in word.lower()])
