772fd7267b63f85ddd9ffbb8df8760e451afbee2
Jakub Samul <jakubsamul0@gmail.com>
2023-03-22 22:40:01 +0100

    '6kata-Consecutive-strings'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -253,3 +253,19 @@
 #             temp += alpha_dict[letter]
 #         word_value.append(temp)
 #     return max(word_value)
+
+
+
+# Consecutive strings
+# def longest_consec(strarr, k):
+#     n = len(strarr)
+#     if n == 0 or k > n or k <= 0:
+#         return ''
+#     longest = index = 0
+#     for i in range(n - k + 1):
+#         length = sum([len(s) for s in strarr[i: i + k]])
+#         if length > longest:
+#             longest = length
+#             index = i
+#     return ''.join(strarr[index: index + k])
+# assert longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2) == "abigailtheta"
