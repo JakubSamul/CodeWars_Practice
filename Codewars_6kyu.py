d11b032df1603d7fa49bb912acef118517e884e7
Jakub Samul <jakubsamul0@gmail.com>
2023-04-04 21:34:54 +0200

    '6kata-A-Rule-of-Divisibility-by-13'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -275,3 +275,22 @@
 # Count characters in your string
 # def count(s):
 #     return {c:s.count(c) for c in s}
+
+
+
+# A Rule of Divisibility by 13
+# def thirt(n):
+#     pattern = [1, 10, 9, 12, 3, 4]
+#     sum = 0
+
+#     while True:
+#         current_sum = 0
+#         for index, digit in enumerate(str(n)[::-1]):
+#             current_index = index % len(pattern)
+#             current_sum += int(digit) * pattern[current_index]
+
+#         if sum == current_sum:
+#             return sum
+
+#         sum = current_sum
+#         n = current_sum
