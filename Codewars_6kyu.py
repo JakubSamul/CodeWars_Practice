b650b054d2b5307445221027f97b3f426f724058
Jakub Samul <jakubsamul0@gmail.com>
2023-11-30 18:04:03 +0100

    'Unique-In-Order'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -294,3 +294,13 @@
 
 #         sum = current_sum
 #         n = current_sum
+
+
+
+# Unique In Order
+# def unique_in_order(sequence):
+#     result = []
+#     for i in sequence:
+#         if len(result) < 1 or not i == result[len(result) - 1]:
+#             result.append(i)
+#     return result
