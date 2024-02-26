b98283294e1032e00eb797dd9fe3947593a425ba
Jakub Samul <jakubsamul0@gmail.com>
2024-02-26 21:22:02 +0100

    'Bouncing-Balls-kata6'

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -357,3 +357,18 @@
 #     return result
 
 # print(multiplication_table(3))
+
+
+# # Bouncing Balls
+# def bouncing_ball(h, bounce, window):
+#     if h <= 0 or bounce <= 0 or bounce >= 1 or window >= h:
+#         return -1
+    
+#     passes = 0
+#     while h > window:
+#         passes += 1
+#         h *= bounce
+#         if h > window:
+#             passes += 1
+    
+#     return passes
