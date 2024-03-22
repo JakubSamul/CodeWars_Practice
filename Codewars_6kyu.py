549d5ec862c59e1b471123a8dae625f6995d14e4
Jakub Samul <jakubsamul0@gmail.com>
2024-03-22 21:11:44 +0100

    Street-Fighter-2-Character-Selection-kata6

--- Codewars_6kyu.py
+++ Codewars_6kyu.py
@@ -401,3 +401,24 @@
 
 
 # print(dashatize(a))
+
+
+# Street Fighter 2 - Character Selection
+def street_fighter_selection(fighters, initial_position, moves):
+    rows, cols = len(fighters), len(fighters[0])
+    current_row, current_col = initial_position
+    selected_fighters = []
+
+    for move in moves:
+        if move == "up":
+            current_row = max(0, current_row - 1)
+        elif move == "down":
+            current_row = min(rows - 1, current_row + 1)
+        elif move == "left":
+            current_col = (current_col - 1) % cols
+        elif move == "right":
+            current_col = (current_col + 1) % cols
+
+        selected_fighters.append(fighters[current_row][current_col])
+
+    return selected_fighters
