74d6c1f0fa2942724089de99b45e42f35d1e069e
JakubSamul <jakubsamul0@gmail.com>
2024-08-28 10:41:40 +0200

    Simple Pig Latin-kata5

--- Codewars_5kyu.py
+++ Codewars_5kyu.py
@@ -173,9 +173,21 @@
 # print(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"))
 
 
-# Moving Zeros To The End
-def move_zeros(array):
-    return sorted(array, key=lambda x: x == 0 and x is not False)
+# # Moving Zeros To The End
+# def move_zeros(array):
+#     return sorted(array, key=lambda x: x == 0 and x is not False)
 
 
-print(move_zeros([0, 1, None, 2, False, 1, 0]))
+# print(move_zeros([0, 1, None, 2, False, 1, 0]))
+
+
+# Simple Pig Latin
+def pig_it(text):
+    return " ".join(
+        word[1:] + word[0] + "ay" if word.isalpha() else word
+        for word in text.split()
+    )
+
+
+print(pig_it("Pig latin is cool"))
+print(pig_it("Hello world !"))
